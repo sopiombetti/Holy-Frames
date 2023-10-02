753be9ba93bf73370b2f9fb010c33ab0db4ad539
sopiombetti <sopiombetti@gmail.com>
2023-10-02 17:09:32 -0300

    testing build

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ import theme from "./src/utils/tailwind/theme"
 // npx tailwindcss -i ./src/utils/tailwind/input.css -o ./dist/output.css --watch
 
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./dist/**/*.{html,js,jsx}"],
   theme: {
     extend: theme,
   },
